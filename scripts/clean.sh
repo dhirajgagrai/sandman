aa3d63ec0fe6b2683790d894519ec8fd1f52ff8a
Dhiraj <dhirajgagrai@iisc.ac.in>
2025-11-16 13:50:46 +0500

    fix: vmlinux.h generated using system files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,6 +7,7 @@ rm -f dfa.dat
 rm -f dfa.dot
 rm -f final-build.out
 
+rm -r ./loader/vmlinux.h
 rm -f ./loader/monitor.o
 rm -f ./loader/monitor.skel.h
 rm -f ebpf-loader
--- scripts/ebpf-build.sh
+++ scripts/ebpf-build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -e
 
+bpftool btf dump file /sys/kernel/btf/vmlinux format c > ./loader/vmlinux.h
 clang -g -O2 -target bpf -c ./loader/monitor.c -o ./loader/monitor.o
 bpftool gen skeleton ./loader/monitor.o > ./loader/monitor.skel.h
 clang ./loader/loader.c -o ebpf-loader -lbpf -lelf
 
+rm ./loader/vmlinux.h
 rm ./loader/monitor.o
 rm ./loader/monitor.skel.h
