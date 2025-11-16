19e78ac4edd926bfc3f1e201e7bd3e028dc9a28c
Dhiraj <dhirajgagrai@iisc.ac.in>
2025-11-16 13:53:23 +0500

    fix: typo in clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,11 +3,11 @@
 rm -rf ./build/*
 rm -rf ./.cache
 
-rm -f dfa.dat
-rm -f dfa.dot
+rm -f nfa.dat
+rm -f nfa.dot
 rm -f final-build.out
 
-rm -r ./loader/vmlinux.h
+rm -f ./loader/vmlinux.h
 rm -f ./loader/monitor.o
 rm -f ./loader/monitor.skel.h
 rm -f ebpf-loader
